0a7f1000a66a5059b0a00abaf1a9dd23f52e1997
thuviengithub0208 <khoaphp@yahoo.com>
2018-08-01 18:47:13 +0700

    Initial commit

---
+++ app/src/main/java/com/ptp/phamtanphat/fragmentpopbackstack0405/FragmentAndroid.java
@@ -0,0 +1,20 @@
+package com.ptp.phamtanphat.fragmentpopbackstack0405;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentAndroid extends Fragment{
+
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_android,container,false);
+        return view;
+    }
+}
