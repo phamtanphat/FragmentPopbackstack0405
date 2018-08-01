cdea12e1abaf8c9a3465fa101b398c42efc08b2f
thuviengithub0208 <khoaphp@yahoo.com>
2018-08-01 19:05:30 +0700

    "Update"

--- app/src/main/java/com/ptp/phamtanphat/fragmentpopbackstack0405/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/fragmentpopbackstack0405/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,13 +20,36 @@ public class MainActivity extends AppCompatActivity {
     public void AddAndroid(View v){
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         FragmentAndroid fragmentAndroid = new FragmentAndroid();
-        fragmentTransaction.add(R.id.relativelayout,fragmentAndroid);
+        fragmentTransaction.add(R.id.relativelayout,fragmentAndroid,"fragmentAndroid");
         fragmentTransaction.commit();
     }
     public void Addios(View v){
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         Fragmentios fragmentios = new Fragmentios();
-        fragmentTransaction.add(R.id.relativelayout,fragmentios);
+        fragmentTransaction.add(R.id.relativelayout,fragmentios,"fragmentios");
         fragmentTransaction.commit();
+    }
+    public void ReplaceAndroid(View v){
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentAndroid fragmentAndroid = new FragmentAndroid();
+        fragmentTransaction.replace(R.id.relativelayout,fragmentAndroid);
+        fragmentTransaction.commit();
+    }
+    public void Replaceios(View v){
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragmentios fragmentios = new Fragmentios();
+        fragmentTransaction.replace(R.id.relativelayout,fragmentios);
+        fragmentTransaction.commit();
+    }
+
+    public void RemoveAndroid(View v){
+        FragmentAndroid fragmentAndroid = (FragmentAndroid) getSupportFragmentManager().findFragmentByTag("fragmentAndroid");
+        if (fragmentAndroid != null){
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(fragmentAndroid);
+            fragmentTransaction.commit();
+        }else {
+            Toast.makeText(this, "Khong fragment android", Toast.LENGTH_SHORT).show();
+        }
     }
 }
